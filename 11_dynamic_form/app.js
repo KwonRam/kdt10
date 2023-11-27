530b4c3a7de50648425c081f64804217883a2f01
KwonRam <kms971226@gmail.com>
2023-11-27 09:10:02 +0900

    ah

--- 11_dynamic_form/app.js
+++ 11_dynamic_form/app.js
@@ -8,10 +8,15 @@ app.set('views', './views')
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.gett('/',(req, res)=> {
-    res.render('index');
+app.get('/',(req, res)=> {
+    res.render('practice1');
 })
 
+app.get('/axios', (req, res)=> {
+    console.log(req.query);
+    res.send(req.query);
+})
+/*
 app.get('/ajax', (req, res) => {
     console.log(req.query);
     res.send(req.query);
@@ -30,7 +35,7 @@ app.post('/fetch', (req, res) => {
     console.log(req.body);
     res.send(req.body);
 })
-
+*/
 
 app.listen(PORT, () => {
     console.log(`server is opening ${PORT}`);
