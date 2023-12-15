79866fe50b9309d2419bae47cfe52071847d6f9c
KwonRam <kms971226@gmail.com>
2023-12-15 12:44:30 +0900

    d

---
+++ book_page/routes/bookMain.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('./controller/CbookDescription');
+
+// localhost:PORT/user 기본 경로
+
+router.get('/', controller.main);
+router.get('/bookshelf', controller.get_bookshelf);
+/*router.get('/book/:isbn', controller.get_description);*/
+
+module.exports = router;
