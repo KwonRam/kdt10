b303f1d4c844f6503f5723f4d943786d0caf6fc4
KwonRam <kms971226@gmail.com>
2024-01-18 12:16:59 +0900

    준왼게임8

---
+++ junsu_game_demo_release/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import CombatDemoUpload from './combat-demo-for-upload';
+
+function App() {
+  return <CombatDemoUpload />;
+}
+
+export default App;
---
+++ junsu_game_demo_release/src/utils/util.js
@@ -0,0 +1,5 @@
+export function dropNumber(number) {
+  number = Math.floor(number * 100);
+  number = number / 100;
+  return;
+}
