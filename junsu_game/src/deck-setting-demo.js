c2b7f4b979d8cac34e236ec6ee75eb1590adeda7
KwonRam <kms971226@gmail.com>
2024-01-20 14:01:56 +0900

    준왼게임11

--- junsu_game/src/deck-setting-demo.js
+++ junsu_game/src/deck-setting-demo.js
@@ -21,6 +21,7 @@ function DeckSetting() {
       isAdjutant: false,
       isEnemy: false,
       isSixMan: false,
+      onBattlefield: false,
       passiveSkill: '엘리트 출신',
       activeSkillFirst: '할 때는 한다',
       activeSkillSecond: '착각하지 마',
@@ -69,6 +70,7 @@ function DeckSetting() {
       isAdjutant: false,
       isEnemy: false,
       isSixMan: false,
+      onBattlefield: false,
       passiveSkill: '바람의 힘으로!',
       activeSkillFirst: '최고의 응원!',
       activeSkillSecond: '한 번 더 너에게',
@@ -117,6 +119,7 @@ function DeckSetting() {
       isAdjutant: false,
       isEnemy: false,
       isSixMan: false,
+      onBattlefield: false,
       passiveSkill: '젊은 멘토',
       activeSkillFirst: '타임아웃',
       activeSkillSecond: '멤버 교체',
@@ -165,6 +168,7 @@ function DeckSetting() {
       isAdjutant: true,
       isEnemy: false,
       isSixMan: false,
+      onBattlefield: false,
       passiveSkill: '플레이 메이커',
       activeSkillFirst: '내만 믿어라',
       activeSkillSecond: '직접 할 차례다',
@@ -213,6 +217,7 @@ function DeckSetting() {
       isAdjutant: false,
       isEnemy: false,
       isSixMan: false,
+      onBattlefield: false,
       passiveSkill: '넘어설 벽',
       activeSkillFirst: '찢어버리기',
       activeSkillSecond: '동기부여가 잘 돼',
@@ -261,6 +266,7 @@ function DeckSetting() {
       isAdjutant: false,
       isEnemy: false,
       isSixMan: false,
+      onBattlefield: false,
       passiveSkill: '드래곤의 피지컬',
       activeSkillFirst: '드래곤 스매셔',
       activeSkillSecond: '하늘을 향해',
@@ -309,6 +315,7 @@ function DeckSetting() {
       isAdjutant: false,
       isEnemy: false,
       isSixMan: false,
+      onBattlefield: false,
       passiveSkill: '늑대정신',
       activeSkillFirst: '최고의 디펜더',
       activeSkillSecond: '살아남을 거야',
@@ -357,6 +364,7 @@ function DeckSetting() {
       isAdjutant: false,
       isEnemy: false,
       isSixMan: false,
+      onBattlefield: false,
       passiveSkill: '12분',
       activeSkillFirst: '제법인데?',
       activeSkillSecond: '걱정마세요',
@@ -405,6 +413,7 @@ function DeckSetting() {
       isAdjutant: false,
       isEnemy: false,
       isSixMan: false,
+      onBattlefield: false,
       passiveSkill: '언럭키',
       activeSkillFirst: '사륜안!',
       activeSkillSecond: '왼팔에 흑염룡이...',
@@ -453,6 +462,7 @@ function DeckSetting() {
       isAdjutant: false,
       isEnemy: false,
       isSixMan: false,
+      onBattlefield: false,
       passiveSkill: '원래는 마법사가...',
       activeSkillFirst: '길을 찾고 싶어',
       activeSkillSecond: '마르세유 턴',
@@ -487,8 +497,8 @@ function DeckSetting() {
       charNo: 11,
       id: 0,
       charName: '서은재',
-      HPcurrent: 99999,
-      HPstat: 99999,
+      HPcurrent: 26,
+      HPstat: 26,
       physicResist: 3,
       magicResist: 3,
       attack: 6,
@@ -501,6 +511,7 @@ function DeckSetting() {
       isAdjutant: false,
       isEnemy: false,
       isSixMan: false,
+      onBattlefield: false,
       passiveSkill: '응원단',
       activeSkillFirst: '우등생',
       activeSkillSecond: '비법노트',
@@ -550,6 +561,7 @@ function DeckSetting() {
     isAdjutant: false,
     isEnemy: false,
     isSixMan: false,
+    onBattlefield: false,
     passiveSkill: '',
     activeSkillFirst: '',
     activeSkillSecond: '',
@@ -595,8 +607,8 @@ function DeckSetting() {
     console.log('useEffect charstatdetail ', characterStatDetail);
   }, [characterStatDetail]);
 
-  const [position, setPosition] = useState('1-1-1');
-  const [positionArray, setPositionArray] = useState([1, 1, 1]);
+  const [position, setPosition] = useState('1-2-2');
+  const [positionArray, setPositionArray] = useState([1, 2, 2]);
 
   const onChangePosition = (event) => {
     setPosition(event.target.value);
@@ -609,11 +621,13 @@ function DeckSetting() {
   useEffect(() => {
     console.log('useEffect position ', position);
   }, [position]);
+
   useEffect(() => {
     console.log('useEffect positionArray ', positionArray);
   }, [position]);
 
   const [selectChar, setSelectChar] = useState(forReset);
+
   useEffect(() => {
     console.log('useEffect selectChar ', selectChar);
   }, [selectChar]);
@@ -624,8 +638,201 @@ function DeckSetting() {
     setSelectChar(item);
     console.log('setSelectChar ', item);
   };
+  //캐릭터의 위치정보 업데이트
+  const [characterPositions, setCharacterPositions] = useState([
+    {
+      charNo: 0,
+      line: 'Buffer',
+      Position: 0,
+      htmlElem: <div></div>,
+    },
+    {
+      charNo: 0,
+      line: 'Buffer',
+      Position: 1,
+      htmlElem: <div></div>,
+    },
+    {
+      charNo: 0,
+      line: 'Buffer',
+      Position: 2,
+      htmlElem: <div></div>,
+    },
+    {
+      charNo: 0,
+      line: 'Buffer',
+      Position: 3,
+      htmlElem: <div></div>,
+    },
+    {
+      charNo: 0,
+      line: 'Buffer',
+      Position: 4,
+      htmlElem: <div></div>,
+    },
+    {
+      charNo: 0,
+      line: 'Buffer',
+      Position: 5,
+      htmlElem: <div></div>,
+    },
+    {
+      charNo: 0,
+      line: 'Buffer',
+      Position: 6,
+      htmlElem: <div></div>,
+    },
+    {
+      charNo: 0,
+      line: 'Buffer',
+      Position: 7,
+      htmlElem: <div></div>,
+    },
+    {
+      charNo: 0,
+      line: 'Buffer',
+      Position: 8,
+      htmlElem: <div></div>,
+    },
+    {
+      charNo: 0,
+      line: 'Buffer',
+      Position: 9,
+      htmlElem: <div></div>,
+    },
+    {
+      charNo: 0,
+      line: 'Buffer',
+      Position: 10,
+      htmlElem: <div></div>,
+    },
+    {
+      charNo: 0,
+      line: 'Buffer',
+      Position: 11,
+      htmlElem: <div></div>,
+    },
+    {
+      charNo: 0,
+      line: 'Buffer',
+      Position: 12,
+      htmlElem: <div></div>,
+    },
+  ]);
+  useEffect(() => {
+    console.log('useEffect characterPositions ', characterPositions);
+  }, [characterPositions]);
+
+  // 캐릭터의 위치를 업데이트하는 함수
+  const updateCharacterPosition = (line, position, selectChar) => {
+    if (selectChar.charName === 'Buffer') {
+      console.log('제발아무것도하지마');
+      return characterPositions;
+    } else {
+      let updatedPositions = [...characterPositions];
+      console.log(updatedPositions, ' updatedPositions');
+      const newPosition = {
+        charNo: selectChar.charNo,
+        Line: line,
+        Position: position,
+        htmlElem: <div></div>,
+      };
+      let selectCharTemp = forReset;
+      //선택한 캐릭터가 들어가려고 하는 슬롯 위치
+      let updateTarget = updatedPositions.find(
+        (elem) => elem.Position === position
+      );
+      let updateIndex = updatedPositions.indexOf(updateTarget);
+      console.log('updateIndex ', updateIndex);
+      //슬롯에 두번 들어가나 체크
+      for (let k = 0; k < updatedPositions.length; k++) {
+        console.log(updatedPositions[k].charNo, '아니시발뭐야?');
+      }
+      let reduplicationCheck = updatedPositions.filter(
+        (elem) => elem.charNo === selectChar.charNo
+      );
+      console.log('중복 체크가 왜 안되냐고오오오 ', reduplicationCheck);
+      if (reduplicationCheck.length < 1) {
+        newPosition.htmlElem = (
+          <img
+            src={process.env.PUBLIC_URL + `/img/${selectChar.img}.png`}
+            style={{ height: '140px' }}
+            alt={`${selectChar.img}`}
+            onClick={() => onShowStatDetail(selectChar.charNo)}
+            onDragStart={() => onDragCharacter(selectChar)}
+          />
+        );
+        console.log(position);
+        console.log(typeof position);
+
+        updatedPositions = updatedPositions.filter(
+          (i) => i.Position !== position
+        );
+        console.log('updatedPositions', updatedPositions);
+        updatedPositions.push(newPosition);
+        console.log(updatedPositions, ' updatedPositions');
+        setCharacterPositions(updatedPositions);
+        selectCharTemp.Line = newPosition.Line;
+        selectCharTemp.Position = newPosition.Position;
+        selectCharTemp.onBattlefield = true;
+        setSelectChar(selectCharTemp);
+        let selectedIndex = myDeckData.findIndex((i) => i === selectChar);
+        let myDeckDataCopy = myDeckData;
+        myDeckDataCopy[selectedIndex] = selectCharTemp;
+        setMyDeckData(myDeckDataCopy);
+        return characterPositions;
+      } else {
+        if (reduplicationCheck.length === 1) {
+          console.log(reduplicationCheck.length, ' 1인지 체크');
+          console.log(
+            selectChar.charNo,
+            ' 들어가려고 하는 캐릭터의 고유번호 체크'
+          );
+          console.log(
+            updateTarget.charNo,
+            ' 들어가려고 하는 슬롯에 있는 캐릭터 고유번호 체크'
+          );
+          let switcherNum = selectChar.charNo;
+          let switcher = selectChar;
+          let switcherIndex = characterPositions.findIndex(
+            (i) => i.charNo === switcherNum
+          );
+          console.log('들어가려고 하는 ', switcherNum, switcher, switcherIndex);
+          let slotOwnerNum = updateTarget.charNo;
+          let slotOwner = characterPositions.find(
+            (i) => i.charNo === slotOwnerNum
+          );
+          let slotOwnerIndex = characterPositions.findIndex(
+            (i) => i.charNo === slotOwnerNum
+          );
+          console.log('자리 주인 ', slotOwnerNum, slotOwner, slotOwnerIndex);
+          let myDeckDataCopy = myDeckData;
+          let characterPositionsCopy = characterPositions;
+        } else {
+          alert('중복 출전은 불가능합니다!');
+          return characterPositions;
+        }
+      }
+    }
+  };
+
   const onDragLine = (e) => {
-    console.log('drop position', e.target);
+    if (e.target.childNodes[0] !== undefined) {
+      let line = e.target.childNodes[0].data;
+      let position = e.target.childNodes[1].data;
+      updateCharacterPosition(line, Number(position), selectChar);
+    } else {
+      console.log('뭐야ㅁㅊ');
+      console.log(e.target.parentNode);
+      console.log(e.target.parentNode.childNodes[0]);
+      console.log(e.target.parentNode.childNodes[2]);
+      let line = e.target.parentNode.childNodes[0].data;
+      let position = e.target.parentNode.childNodes[1].data;
+      characterPositions[position].htmlElem = <div></div>;
+      console.log(characterPositions[position].charNo);
+      console.log('왜 선택이 안되지 ', selectChar);
+      updateCharacterPosition(line, Number(position), selectChar);
+    }
   };
 
   return (
@@ -658,7 +865,8 @@ function DeckSetting() {
                   <div
                     style={{
                       border:
-                        characterStatDetail.charNo === item.charNo
+                        characterStatDetail.charNo === item.charNo &&
+                        item.img !== ''
                           ? '2px solid red'
                           : '2px solid transparent',
                     }}
@@ -730,6 +938,7 @@ function DeckSetting() {
         <div className="frontLine">
           <p>전열: 탱커 or 근접 딜러를 배치하세요</p>
           <div>
+            <p>슬롯1</p>
             <div
               onDrop={(e) => onDragLine(e)}
               onDragOver={onDragOver}
@@ -738,10 +947,16 @@ function DeckSetting() {
                 backgroundColor: 'lightblue',
                 display: positionArray[0] >= 1 ? 'block' : 'none',
               }}
-              value="front"
+              value={'front'}
             >
-              <p>슬롯1</p>
+              {'front'}
+              {1}
+              {
+                characterPositions.find((element) => element.Position === 1)
+                  .htmlElem
+              }
             </div>
+            <p>슬롯2</p>
             <div
               onDrop={(e) => onDragLine(e)}
               onDragOver={onDragOver}
@@ -750,10 +965,16 @@ function DeckSetting() {
                 backgroundColor: 'lightblue',
                 display: positionArray[0] >= 2 ? 'block' : 'none',
               }}
-              value="front"
+              value={'front'}
             >
-              <p>슬롯2</p>
+              {'front'}
+              {2}
+              {
+                characterPositions.find((element) => element.Position === 2)
+                  .htmlElem
+              }
             </div>
+            <p>슬롯3</p>
             <div
               onDrop={(e) => onDragLine(e)}
               onDragOver={onDragOver}
@@ -762,15 +983,21 @@ function DeckSetting() {
                 backgroundColor: 'lightblue',
                 display: positionArray[0] >= 3 ? 'block' : 'none',
               }}
-              value="front"
+              value={'front'}
             >
-              <p>슬롯3</p>
+              {'front'}
+              {3}
+              {
+                characterPositions.find((element) => element.Position === 3)
+                  .htmlElem
+              }
             </div>
           </div>
         </div>
         <div className="midLine">
           <p>중열: 근접 딜러 or 서포터를 배치하세요</p>
           <div>
+            <p>슬롯1</p>
             <div
               onDrop={(e) => onDragLine(e)}
               onDragOver={onDragOver}
@@ -779,10 +1006,16 @@ function DeckSetting() {
                 backgroundColor: 'lightblue',
                 display: positionArray[1] >= 1 ? 'block' : 'none',
               }}
-              value="mid"
+              value={'mid'}
             >
-              <p>슬롯1</p>
+              {'mid'}
+              {4}
+              {
+                characterPositions.find((element) => element.Position === 4)
+                  .htmlElem
+              }
             </div>
+            <p>슬롯2</p>
             <div
               onDrop={(e) => onDragLine(e)}
               onDragOver={onDragOver}
@@ -791,10 +1024,16 @@ function DeckSetting() {
                 backgroundColor: 'lightblue',
                 display: positionArray[1] >= 2 ? 'block' : 'none',
               }}
-              value="mid"
+              value={'mid'}
             >
-              <p>슬롯2</p>
+              {'mid'}
+              {5}
+              {
+                characterPositions.find((element) => element.Position === 5)
+                  .htmlElem
+              }
             </div>
+            <p>슬롯3</p>
             <div
               onDrop={(e) => onDragLine(e)}
               onDragOver={onDragOver}
@@ -803,15 +1042,21 @@ function DeckSetting() {
                 backgroundColor: 'lightblue',
                 display: positionArray[1] >= 3 ? 'block' : 'none',
               }}
-              value="mid"
+              value={'mid'}
             >
-              <p>슬롯3</p>
+              {'mid'}
+              {6}
+              {
+                characterPositions.find((element) => element.Position === 6)
+                  .htmlElem
+              }
             </div>
           </div>
         </div>
         <div className="backLine">
           <p>후열: 원거리 딜러 or 서포터를 배치하세요</p>
           <div>
+            <p>슬롯1</p>
             <div
               onDrop={(e) => onDragLine(e)}
               onDragOver={onDragOver}
@@ -820,10 +1065,16 @@ function DeckSetting() {
                 backgroundColor: 'lightblue',
                 display: positionArray[2] >= 1 ? 'block' : 'none',
               }}
-              value="back"
+              value={'back'}
             >
-              <p>슬롯1</p>
+              {'back'}
+              {7}
+              {
+                characterPositions.find((element) => element.Position === 7)
+                  .htmlElem
+              }
             </div>
+            <p>슬롯2</p>
             <div
               onDrop={(e) => onDragLine(e)}
               onDragOver={onDragOver}
@@ -832,10 +1083,16 @@ function DeckSetting() {
                 backgroundColor: 'lightblue',
                 display: positionArray[2] >= 2 ? 'block' : 'none',
               }}
-              value="back"
+              value={'back'}
             >
-              <p>슬롯2</p>
+              {'back'}
+              {8}
+              {
+                characterPositions.find((element) => element.Position === 8)
+                  .htmlElem
+              }
             </div>
+            <p>슬롯3</p>
             <div
               onDrop={(e) => onDragLine(e)}
               onDragOver={onDragOver}
@@ -844,9 +1101,14 @@ function DeckSetting() {
                 backgroundColor: 'lightblue',
                 display: positionArray[2] >= 3 ? 'block' : 'none',
               }}
-              value="back"
+              value={'back'}
             >
-              <p>슬롯3</p>
+              {'back'}
+              {9}
+              {
+                characterPositions.find((element) => element.Position === 9)
+                  .htmlElem
+              }
             </div>
           </div>
         </div>
@@ -856,13 +1118,19 @@ function DeckSetting() {
           <p>전투중인 멤버와 교체되거나</p>
           <p> 서포팅 스킬을 사용할 수 있습니다</p>
           <div>
+            <p>슬롯1</p>
             <div
               onDrop={(e) => onDragLine(e)}
               onDragOver={onDragOver}
               style={{ height: '200px', backgroundColor: 'lightblue' }}
-              value="six"
+              value={'six'}
             >
-              <p>슬롯1</p>
+              {'six'}
+              {12}
+              {
+                characterPositions.find((element) => element.Position === 12)
+                  .htmlElem
+              }
             </div>
           </div>
         </div>
