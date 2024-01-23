ce77425fa9631f7a8b4451966db42b82b33908a4
KwonRam <kms971226@gmail.com>
2024-01-23 10:05:35 +0900

    18

---
+++ 240123-2-proxy/server/server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+const PORT = process.env.PORT || 4000;
+
+app.get('/api/host', (req, res) => {
+  res.send({ host: 'adfasfd' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server On : http://localhost:${PORT}/`);
+});
