1b3423dd3aa408c0f755c8c989e8faf9d44603d1
KwonRam <kms971226@gmail.com>
2024-01-29 09:07:04 +0900

    25

--- 240123-2-proxy-junsu-game/src/BattleMaps/mapData.js
+++ 240123-2-proxy-junsu-game/src/BattleMaps/mapData.js
@@ -3,12 +3,47 @@ module.exports = {
     {
       chapter: 'chapter1',
       id: '1',
-      name: 'map#1',
+      name: 'map1-1',
+    },
+    {
+      chapter: 'chapter1',
+      id: '2',
+      name: 'map1-2',
+    },
+    {
+      chapter: 'chapter1',
+      id: '3',
+      name: 'map1-3',
     },
     {
       chapter: 'chapter2',
-      id: '2',
-      name: 'map#2',
+      id: '4',
+      name: 'map2-1',
+    },
+    {
+      chapter: 'chapter2',
+      id: '5',
+      name: 'map2-2',
+    },
+    {
+      chapter: 'chapter2',
+      id: '6',
+      name: 'map2-3',
+    },
+    {
+      chapter: 'chapter3',
+      id: '7',
+      name: 'map3-1',
+    },
+    {
+      chapter: 'chapter3',
+      id: '8',
+      name: 'map3-2',
+    },
+    {
+      chapter: 'chapter3',
+      id: '9',
+      name: 'map3-3',
     },
   ],
 };
