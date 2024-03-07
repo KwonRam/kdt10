073171896577bc03d7964341613583725157976f
KwonRam <kms971226@gmail.com>
2024-03-07 09:54:08 +0900

    ahhhhhhhh

--- NaiClover-develop/NaiClover-develop/client/src/styles/socket.d.ts
+++
@@ -1,7 +0,0 @@
-import { Socket } from 'socket.io';
-
-interface CustomSocket extends Socket {
-    userId?: string;
-}
-
-export default CustomSocket;
--- NaiClover-develop/NaiClover-develop/client/src/utils/cookieConfig.ts
+++
@@ -1,6 +0,0 @@
-export const cookieConfig: Object = {
-    path: '/',
-    // 단위는 초
-    maxAge: 14 * 24 * 60 * 60,
-    httponly: true,
-};
